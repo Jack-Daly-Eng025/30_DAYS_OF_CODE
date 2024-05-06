79b9af89384b5568f711148a80e134a24b1106fa
Jack-Daly-Eng025 <jac.daly.2021@outlook.com>
2024-05-06 19:17:25 +0100

    changed script again

--- 2_30/src/script.js
+++ 2_30/src/script.js
@@ -1,5 +1,6 @@
+import "./style.css";
 
-import * as THREE from './three';
+import * as THREE from "./node_modules/three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
