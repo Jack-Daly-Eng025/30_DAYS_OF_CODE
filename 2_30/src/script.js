a1137da7848c0323b752ef7c322053c13efddb3b
Jack-Daly-Eng025 <jac.daly.2021@outlook.com>
2024-05-07 15:52:55 +0100

    Changed shape and added a bit of randomness to the color whenever page reloads.

--- 2_30/src/script.js
+++ 2_30/src/script.js
@@ -3,25 +3,44 @@
 /* We don't don't question why it works, we just hope it does */
 import * as THREE from "https://cdn.skypack.dev/three@0.148.0"
 
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector('#bg'), 
+
+});
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+/*Play around with geometry a little*/
+function randColor (){
+	var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+	return randomColor;
+
+}
+
+var shapecolor = randColor();
+const geometry = new  THREE.TorusKnotGeometry(0.99,1.55,12.5,20.5); 
+const material = new THREE.MeshBasicMaterial( { color: shapecolor, wireframe: true} );
+const tknot = new THREE.Mesh( geometry, material );
+scene.add( tknot );
+
 
 camera.position.z = 5;
+
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	tknot.rotation.x += 0.005;
+	tknot.rotation.y += 0.001;
+	tknot.rotation.z += 0.005;
 
 	renderer.render( scene, camera );
 }
